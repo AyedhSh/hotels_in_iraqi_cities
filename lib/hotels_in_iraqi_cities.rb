8c216eea158699c2f0946f07d87fa1a69c78f4a6
AyedhSh <alshmary_ad@yahoo.com>
2017-01-31 15:48:55 +0300

    hotels_in_iraqi_cities.rb is built up for gathering the requirements

---
+++ lib/hotels_in_iraqi_cities.rb
@@ -0,0 +1,10 @@
+require_relative "./city.rb"
+require_relative "./cline.rb"
+require_relative "./hotel.rb"
+require_relative "./scrap.rb"
+
+require 'colorize'
+
+require 'open-uri'
+require 'pry'
+require 'nokogiri'
